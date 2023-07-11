194cc360df9c7f0e325cb8f8e4393dc1e6f39d2d
SadikhovEmin <eemin.sadykhov@gmail.com>
2023-07-11 11:05:45 +0200

    Exercise 3

--- exercise-3/product-descp-service/product_descp.js
+++ exercise-3/product-descp-service/product_descp.js
@@ -1,12 +1,29 @@
 module.exports = function (options) {
     //Import the mock data json file
-    const mockData = require('./MOCK_DATA.json');
+    const mockData = require("./MOCK_DATA.json");
 
     //Add the patterns and their corresponding functions
-    this.add('role:product,cmd:getProductURL', productURL);
-    this.add('role:product,cmd:getProductName', productName);
-
+    this.add("role:product,cmd:getProductURL", productURL);
+    this.add("role:product,cmd:getProductName", productName);
 
     //To DO: add the pattern functions and describe the logic inside the function
+    function productURL(args, done) {
+        var productId = args.productId;
 
-}
+        var res = mockData.find((p) => p["product_id"] === productId)[
+            "productURL"
+        ];
+
+        done(null, { result: res });
+    }
+
+    function productName(args, callBack) {
+        var productId = args.productId;
+
+        var res = mockData.find((p) => p["product_id"] === productId)[
+            "product_name"
+        ];
+
+        done(null, { result: res });
+    }
+};
--- exercise-3/product-price-service/product_price.js
+++ exercise-3/product-price-service/product_price.js
@@ -1,9 +1,17 @@
 module.exports = function (options) {
     //Import the mock data json file
-    const mockData = require('./MOCK_DATA.json');
+    const mockData = require("./MOCK_DATA.json");
     //To DO: Add the patterns and their corresponding functions
-
+    this.add("role:product, getProductPrice", productPrice);
 
     //To DO: add the pattern functions and describe the logic inside the function
+    function productPrice(args, done) {
+        var productId = args.productId;
 
-}
+        var res = mockData.find((p) => p["product_id"] === productId)[
+            "product_price"
+        ];
+
+        done(null, { result: res });
+    }
+};
